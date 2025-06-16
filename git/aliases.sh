942820d709690987cb7a5f3dc6bda4ed58c46465
Manas Harbola <manasharbola@gmail.com>
2025-06-16 19:12:51 -0400

    init commit

---
+++ git/aliases.sh
@@ -0,0 +1,12 @@
+# Git Stuff
+alias gs="git status"
+alias gb="git branch"
+alias gbc="git branch --show-current"
+alias gc="git checkout"
+alias gcb="git checkout -b"
+alias gadd="git add"
+alias gcomm="git commit -m"
+alias gpull="git pull"
+alias gpush="git push"
+alias gpushu='git push -u origin `git branch --show-current`'
+alias gdiff="git diff"
